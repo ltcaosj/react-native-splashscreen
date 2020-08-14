ea551f37ec0b52c7b038fe47c8c855e313995fbb
Cao Thanh Long <ltcaosj@gmail.com>
2020-08-14 14:40:27 +0700

    upgraded

--- android/src/main/java/lightkits/splashscreen/SplashActivity.java
+++ android/src/main/java/lightkits/splashscreen/SplashActivity.java
@@ -2,6 +2,8 @@ package lightkits.splashscreen;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
@@ -9,6 +11,9 @@ public class SplashActivity extends AppCompatActivity {
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
         Intent starter = new Intent(this, LKSplashScreenConfiguration.mainActivityClass);
         this.startActivity(starter);
         //
--- android/src/main/java/lightkits/splashscreen/SplashDialog.java
+++ android/src/main/java/lightkits/splashscreen/SplashDialog.java
@@ -4,12 +4,18 @@ import android.app.Dialog;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+
 import androidx.annotation.NonNull;
 
 public class SplashDialog extends Dialog {
 
   public SplashDialog(@NonNull Context context) {
     super(context, R.style.Theme_AppCompat_Light_NoActionBar);
+    //
+    getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    //
     this.configure();
   }
 
